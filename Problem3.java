8ae37bad26257c30bbcad3951b11fa73ff67b6eb
Manny <mjl288@scarletmail.rutgers.edu>
2013-04-18 12:53:52 -0400

    started problem 3

---
+++ Problem3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Problem3{
+	public static void main(String[] args){
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Bison");
+		System.out.println(list);
+
+	}
+}
