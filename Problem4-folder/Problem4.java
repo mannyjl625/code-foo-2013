318d121e1975da8c3eb001d020cd1359df97bc1e
Manny <mjl288@scarletmail.rutgers.edu>
2013-04-25 13:06:15 -0400

    first commit on new branch. implementing dfs

--- Problem4-folder/Problem4.java
+++ Problem4-folder/Problem4.java
@@ -41,6 +41,53 @@ class Person{
  *Used for the breadth-first search
  */
 
+
+public class Stack<T> {
+
+	
+	private ArrayList<T> items;
+
+	
+	public Stack() {
+		items = new ArrayList<T>();
+	}
+
+	public void push(T item) {
+		items.add(item);
+	}
+
+	
+	public T pop() 
+	throws NoSuchElementException {
+		if (items.isEmpty()) {
+			throw new NoSuchElementException("can't pop from an empty stack");
+		}
+		return items.remove(items.size()-1);
+	}
+
+	public T peek() 
+	throws NoSuchElementException {
+		if (items.size() == 0) {
+			throw new NoSuchElementException("can't peek into an empty stack");
+		}
+		return items.get(items.size()-1);
+	}
+
+	
+	public boolean isEmpty() {
+		return items.isEmpty();
+	}
+
+	
+	public int size() {
+		return items.size();
+	}
+
+	
+	public void clear() {
+		items.clear();
+	}
+}
 class Queue<T> {
 	
 	class CLLNode<E> {
@@ -230,6 +277,7 @@ public class Problem4{
 		//beginning of breadth-first search on familiy tree
 		//traverses through all family members, storing those that match by name and/or generation in results arrayList
 		ArrayList<Person> results = new ArrayList<Person>();
+		/*
 		Queue<Person> q = new Queue<Person>();
 		q.enqueue(root);
 		root.visited = true;
@@ -251,7 +299,7 @@ public class Problem4{
 					q.enqueue(relative);
 				}
 			}			
-		}
+		}*/
 
 		//no familiy members found 
 		if(results.isEmpty()){
